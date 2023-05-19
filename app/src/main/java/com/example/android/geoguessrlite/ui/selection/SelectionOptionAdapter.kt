b8b7b4b019c99e39297318d6a2dfbd20af58b7bd
tsterbic <tsterbic@hubspot.com>
2023-05-19 15:42:38 +0100

    Samll Recycler refactor

--- app/src/main/java/com/example/android/geoguessrlite/ui/selection/SelectionOptionAdapter.kt
+++ app/src/main/java/com/example/android/geoguessrlite/ui/selection/SelectionOptionAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.android.geoguessrlite.ui.selection
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.geoguessrlite.R
 
-class SelectionOptionAdapter : RecyclerView.Adapter<ButtonItemViewHolder>() {
+class SelectionOptionAdapter : RecyclerView.Adapter<ViewHolder>() {
     var data = listOf<String>()
         set(value) {
             field = value
@@ -15,17 +16,19 @@ class SelectionOptionAdapter : RecyclerView.Adapter<ButtonItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: ButtonItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.buttonView.text = item
+        holder.selectionButton.text = item
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ButtonItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
-            .inflate(R.layout.selection_item_view, parent, false) as Button
-        return ButtonItemViewHolder(view)
+            .inflate(R.layout.selection_item_view, parent, false)
+        return ViewHolder(view)
     }
 }
 
-class ButtonItemViewHolder(val buttonView: Button) : RecyclerView.ViewHolder(buttonView)
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val selectionButton: Button = itemView.findViewById(R.id.selectionButton)
+}

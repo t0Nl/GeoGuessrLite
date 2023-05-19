245cafe0e773235d962f64bf22d045d120918460
tsterbic <tsterbic@hubspot.com>
2023-05-19 15:46:58 +0100

    Small Recycler refactor again

--- app/src/main/java/com/example/android/geoguessrlite/ui/selection/SelectionOptionAdapter.kt
+++ app/src/main/java/com/example/android/geoguessrlite/ui/selection/SelectionOptionAdapter.kt
@@ -18,17 +18,28 @@ class SelectionOptionAdapter : RecyclerView.Adapter<ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.selectionButton.text = item
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.selection_item_view, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 }
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
     val selectionButton: Button = itemView.findViewById(R.id.selectionButton)
+
+    fun bind(item: String) {
+        selectionButton.text = item
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater
+                .inflate(R.layout.selection_item_view, parent, false)
+
+            return ViewHolder(view)
+        }
+    }
 }

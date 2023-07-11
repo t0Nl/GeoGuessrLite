d734ccd0398a8de8bf0d4f5b55e716de8448ea07
tsterbic <tsterbic@hubspot.com>
2023-07-11 21:05:38 +0100

    Added request fro notification

--- app/src/main/java/com/example/android/geoguessrlite/MainActivity.kt
+++ app/src/main/java/com/example/android/geoguessrlite/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.android.geoguessrlite
 
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.geoguessrlite.util.createNotificationChannel
 import com.example.android.geoguessrlite.util.scheduleDailyNotification
 
 class MainActivity : AppCompatActivity() {
+    private var hasNotificationPermissionGranted = false
+
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            hasNotificationPermissionGranted = isGranted
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createNotificationChannel(this)
-        scheduleDailyNotification(this)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            hasNotificationPermissionGranted = true
+        }
+
+        if (hasNotificationPermissionGranted) {
+            createNotificationChannel(this)
+            scheduleDailyNotification(this)
+        }
     }
 }
